b9944d87810b82cc73c3d9c710be08b9090e59ec
RafaelRosaSPgit config --global user.name RafaelRosaSPgit config --global user.username RafaelRosaSP <rafael@pilotomaster.com.br>
2021-01-12 16:35:17 -0300

    iniciando dotnet curso DIO

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace dotNet_DigitalInnovationOne_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World2!");
         }
     }
 }
